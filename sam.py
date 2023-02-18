fff9500206dcd47fe488296aab0ccd31b547c01c
Deepikakalava <125595215+Deepikakalava@users.noreply.github.com>
2023-02-19 00:53:13 +0530

    Update sam.py

--- sam.py
+++ sam.py
@@ -1,18 +1,43 @@
-import streamlit as st
+# Import required packages
 import cv2
+import mediapipe as mp
 import numpy as np
-from streamlit_webrtc import VideoProcessorBase, webrtc_streamer
+import streamlit as st
 
+# Load pre-trained hand tracking model
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-class VideoTransformer(VideoProcessorBase):
-    def transform(self, frame: np.ndarray) -> np.ndarray:
-        # Draw a circle on the video frame
-        img = cv2.circle(frame, (50, 50), 30, (255, 0, 0), 5)
-        return img
+# Define app layout and widgets
+st.title("Hand Tracking App")
+start_button = st.button("Start")
+stop_button = st.button("Stop")
 
+# Define hand tracking function
+def track_hands():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        # Convert image to RGB format
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Flip the image horizontally
+        frame = cv2.flip(frame, 1)
+        # Perform hand tracking on the image
+        results = hands.process(frame)
+        # Display the image with hand tracking overlay
+        annotated_image = frame.copy()
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        st.image(annotated_image, channels="RGB")
+        # Check if the Stop button is clicked
+        if stop_button:
+            cap.release()
+            break
 
-def main():
-    webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
-
-if __name__ == "__main__":
-    main()
+# Start the app
+if start_button:
+    track_hands()
