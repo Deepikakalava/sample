7e5fdae8973b0de0dfb3a46014dd8216323358a9
Deepikakalava <125595215+Deepikakalava@users.noreply.github.com>
2023-02-19 00:35:48 +0530

    Update sam.py

--- sam.py
+++ sam.py
@@ -1,37 +1,48 @@
 import streamlit as st
 import streamlit_webrtc as webrtc
-from av import VideoFrame
+import av
 import numpy as np
 
-class VideoTransformer(webrtc.VideoTransformerBase):
-    def __init__(self):
-        self.video_transformer = None
 
-    def transform(self, frame: VideoFrame) -> np.ndarray:
+class VideoTransformer:
+    def __init__(self):
+        self.to_draw = None
+
+    def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
-        return img
+        # Draw a circle on the video frame
+        img = cv2.circle(img, (50, 50), 30, (255, 0, 0), 5)
+        self.to_draw = img
+        return frame
+
+    def get_drawable_frame(self):
+        return self.to_draw
+
 
 def main():
-    st.title("Camera feed")
+    webrtc_ctx = webrtc.webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
 
-    # Create a video transformer that accesses the camera stream
-    video_transformer = VideoTransformer()
+    if webrtc_ctx.video_transformer:
+        video_transformer = webrtc_ctx.video_transformer
+    else:
+        video_transformer = VideoTransformer()
 
-    # Configure the WebRTC connection
-    rtc_configuration = {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    while True:
+        if webrtc_ctx.video_transformer:
+            video_frame = webrtc_ctx.video_transformer.get_frame()
+        else:
+            # Get the next video frame
+            video_frame = webrtc_ctx.get_next_frame()
 
-    # Create a webrtc streamer with the custom video transformer
-    webrtc_streamer = webrtc.webrtc_streamer(
-        key="example",
-        video_processor_factory=video_transformer,
-        rtc_configuration=rtc_configuration,
-        async_processing=False,
-    )
+            # Update the video transformer with the new frame
+            video_transformer.transform(video_frame)
 
-    # Display the camera feed in the app
-    if webrtc_streamer is not None:
-        video_frame = video_transformer.transformed_frame
-        st.image(video_frame)
+            # Get the transformed frame
+            video_frame = video_transformer.get_drawable_frame()
+
+        # Display the video frame in the Streamlit app
+        st.image(video_frame, channels="BGR", use_column_width=True)
+
 
 if __name__ == "__main__":
     main()
