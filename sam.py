378d52b7aa11f241f41c444296b8f3240990fde2
Deepikakalava <125595215+Deepikakalava@users.noreply.github.com>
2023-02-19 01:14:16 +0530

    Update sam.py

--- sam.py
+++ sam.py
@@ -23,4 +23,4 @@ with mp_hands.Hands(
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
-                    annotated_frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
+                    annotated_frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
