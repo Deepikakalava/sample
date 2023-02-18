1b7096df68735a1f8b60708c2bfd0965edfec30a
Deepikakalava <125595215+Deepikakalava@users.noreply.github.com>
2023-02-19 00:37:07 +0530

    Update sam.py

--- sam.py
+++ sam.py
@@ -4,41 +4,31 @@ import av
 import numpy as np
 
 
-class VideoTransformer:
+class VideoTransformer(webrtc.VideoProcessor):
     def __init__(self):
         self.to_draw = None
 
-    def transform(self, frame):
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
         img = frame.to_ndarray(format="bgr24")
         # Draw a circle on the video frame
         img = cv2.circle(img, (50, 50), 30, (255, 0, 0), 5)
         self.to_draw = img
-        return frame
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
     def get_drawable_frame(self):
         return self.to_draw
 
 
 def main():
-    webrtc_ctx = webrtc.webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+    webrtc_ctx = webrtc.webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
 
-    if webrtc_ctx.video_transformer:
-        video_transformer = webrtc_ctx.video_transformer
+    if webrtc_ctx.video_processor:
+        video_transformer = webrtc_ctx.video_processor
     else:
         video_transformer = VideoTransformer()
 
     while True:
-        if webrtc_ctx.video_transformer:
-            video_frame = webrtc_ctx.video_transformer.get_frame()
-        else:
-            # Get the next video frame
-            video_frame = webrtc_ctx.get_next_frame()
-
-            # Update the video transformer with the new frame
-            video_transformer.transform(video_frame)
-
-            # Get the transformed frame
-            video_frame = video_transformer.get_drawable_frame()
+        video_frame = video_transformer.get_drawable_frame()
 
         # Display the video frame in the Streamlit app
         st.image(video_frame, channels="BGR", use_column_width=True)
